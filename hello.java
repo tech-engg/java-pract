c812b2e07bde417dd5124a4968230d65c02b8229
azeem pasha <82228574+tech-engg@users.noreply.github.com>
2021-05-11 23:57:07 +0530

    helloworld.java
    second commit

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ package com.javapractice;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello rld");
+		System.out.println("hello world");
+		System.out.println("hello java");
 		// TODO Auto-generated method stub
 
 	}
