0d96bc9a87341eaf30ef6887df9b2b039db2f3dd
tech-engg <82228574+tech-engg@users.noreply.github.com>
2021-05-11 09:45:43 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ package com.javapractice;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello rld");
 		// TODO Auto-generated method stub
 
 	}
