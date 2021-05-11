50eabee163f00d7aef0aa0ceef41dea4ae533ced
azeem pasha <82228574+tech-engg@users.noreply.github.com>
2021-05-12 00:31:16 +0530

    added hello java message

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ package com.javapractice;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello java");
+		System.out.println("hello Java");
 		// TODO Auto-generated method stub
 
 	}
